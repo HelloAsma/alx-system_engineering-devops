e634826b440c7812d9e5cf51cec6885fc6133701
pixie-a <aboukhris33@gmail.com>
2024-06-24 02:59:57 +0100

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # automated puppet fix (find out why Apache is returning a 500 error)
 
 exec { 'Fix wordpress':
-  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
